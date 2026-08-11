c9b29e00117296aac6724b09a206e2ee5b91cbd3
Kenji Saito <ken-yo@mbr.nifty.com>
2026-08-11 21:11:01 +0900

    Initial commit

---
+++ update.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+CUR=$(pwd)
+
+CURRENT=$(cd "$(dirname "$0")" || exit;pwd)
+echo "${CURRENT}"
+
+cd "${CURRENT}" || exit
+git pull --prune
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+echo ""
+pwd
+
+if ! (pnpm self-update && pnpm install && pnpm up && pnpm audit --fix override && pnpm up && pnpm lint-fix && pnpm build); then
+  cd "${CUR}" || exit
+  exit 1
+fi
+
+cd "${CURRENT}" || exit
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+git commit -am "Bumps node modules" && git push
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+
+cd "${CUR}" || exit
---
+++ vitest-setup.ts
@@ -0,0 +1,6 @@
+/* eslint-disable stencil/ban-side-effects */
+import { defineCustomElements } from './loader/index.js';
+
+defineCustomElements();
+
+/* eslint-enable stencil/ban-side-effects */
